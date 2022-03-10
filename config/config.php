19d5cf7808acc5a79d86879aac843cd84e15c72e
laxon17 <lazarlalovic@gmail.com>
2022-03-10 22:40:19 +0100

    added config.php

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    return 
+    [
+        'host' => 'localhost',
+        'dbname' => 'Wireframe',
+        'username' => 'root',
+        'password' => 'Laxon!00',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ];
