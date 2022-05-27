a0edf13e979c9bdb79f27699c73936560065c6e0
laxon17 <lazarlalovic@gmail.com>
2022-05-28 01:37:47 +0200

    config updated

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
         'host' => 'localhost',
         'dbname' => 'wireframe',
         'username' => 'lakson',
-        'password' => 'Laxon123',
+        'password' => 'Lakson123',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
